962c396375c89c7acf7e0e61819058fa6b9bdbc8
alexander <skidan2908@gmail.com>
2023-08-16 21:07:50 +0300

    Добавление .env файла

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,12 +7,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = 3000;
+  const port = process.env['PORT'] || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
